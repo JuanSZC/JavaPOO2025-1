14d2896d99bc32f41e3f3f4bffe7f175bd49b964
JuanSZC <juans.zambranoc@uqvirtual.edu.co>
2025-05-21 13:07:17 -0500

    Update

--- BibliotecaJFX/src/main/java/co/edu/uniquindio/poo/bibliotecajfx/viewController/PantallaVisitanteViewController.java
+++ BibliotecaJFX/src/main/java/co/edu/uniquindio/poo/bibliotecajfx/viewController/PantallaVisitanteViewController.java
@@ -1,11 +1,20 @@
 package co.edu.uniquindio.poo.bibliotecajfx.viewController;
 
 import co.edu.uniquindio.poo.bibliotecajfx.App;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PantallaVisitanteViewController {
     App app;
 
     public void setApp(App app) {
         this.app = app;
+    }
+
+    @FXML
+    Button btCerrarSesion;
+
+    public void openPantallaBienvenida() {
+        app.openPantallaBienvenida();
     }
 }
